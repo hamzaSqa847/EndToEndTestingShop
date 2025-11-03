d05a78831b6b67c2eca1bdc69733045feaba2540
hamzaSqa847 <hamzaicp53@gmail.com>
2025-11-03 23:44:46 +0500

    fix

--- tests/about.spec.ts
+++ tests/about.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import { HomePage } from '../pages/HomePage';
 import { AboutPage } from '../pages/AboutPage';
 
-test('VerifyAboutPage', async ({ page }) => {
+test('VerifyAbouttPage', async ({ page }) => {
 const Home = new HomePage(page);
 const About = new AboutPage(page);
 
